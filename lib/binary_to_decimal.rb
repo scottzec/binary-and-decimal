dea85fd23c5d27f517b7d0dd01c4949dd6185ff9
Leah Scott-Zechlin <scottzec@gmail.com>
2020-09-16 23:23:31 +0200

    trying again to upload method

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -2,22 +2,24 @@
 # Calculate  and return the decimal value for this binary number using the algorithm you devised in class.
 
 def binary_to_decimal(binary_array)
-  total = 0
-  binary_array.each do |num|
-    total = 2 * total + num
+  dec_total = 0
+  binary_array.each do |number|
+    dec_total = 2 * dec_total + number
   end
 
-  return total
+  return dec_total
 
 end
 
-
+# Generates random array to pass as argument in binary_to_decimal method
 binary_numbers = Array.new(8) {rand (0..1)}
+
 
 binary_to_decimal(binary_numbers)
 
 
 # Test Arrays
+
 array = [1, 1, 1] #7
 
 arr2 = [1, 0, 0, 1] #9
@@ -25,6 +27,7 @@ arr2 = [1, 0, 0, 1] #9
 arr3 = [1, 0, 1, 1] #11
 
 arr4 = [0, 1, 1, 1] #7
+
 
 # puts binary_to_decimal(array)
 
